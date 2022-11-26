b99c9129ae31a9dad88556d4955a0c8fd6957401
Zakaria Rabou <zakariarabou0@gmail.com>
2022-11-26 19:48:04 +0100

    initial project added

---
+++ src/test/java/emsi/efoot/EfootApplicationTests.java
@@ -0,0 +1,13 @@
+package emsi.efoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
