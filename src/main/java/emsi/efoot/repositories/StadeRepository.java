ab61c0ce1cbd5b4fa1e13c3441d6e811a84e6c3d
Rabou <zakariarabou4@gmail.com>
2023-01-01 01:25:51 +0100

    Adding stadesList functions

--- src/main/java/emsi/efoot/repositories/StadeRepository.java
+++ src/main/java/emsi/efoot/repositories/StadeRepository.java
@@ -4,6 +4,9 @@ import emsi.efoot.entities.Stade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface StadeRepository extends JpaRepository<Stade, Integer> {
+    List<Stade> findByComplexeId(int idComplexe);
 }
